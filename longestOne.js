1ee390b04d064bd57da52f7eff9155667b743454
Philip Browne <pbrowne@gmail.com>
2023-04-02 14:50:17 -0400

    feat: longestOne

---
+++ longestOne.js
@@ -0,0 +1,15 @@
+const longest = (string) => {
+  if (string.length === 0) return 0;
+  let max = 0;
+  let current = 1;
+  for (let i = 1; i < string.length; i++) {
+    if (string[i] === string[i - 1]) {
+      current++; // increment current block length if the streak continues
+    } else {
+      if (current > max) max = current;
+      current = 1;
+    }
+  }
+  if (current > max) max = current;
+  return max;
+};
